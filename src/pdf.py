a3e3c8cfd68bce6242bc815b60b79eb0dae6d873
MacarenaBenguigui <130480569+MacarenaBenguigui@users.noreply.github.com>
2024-03-11 16:31:14 +0100

    Update pdf.py

--- src/pdf.py
+++ src/pdf.py
@@ -62,6 +62,7 @@ hide_streamlit_style = """
             <style>
             [data-testid="stToolbar"] {visibility: hidden !important;}
             footer {visibility: hidden !important;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
